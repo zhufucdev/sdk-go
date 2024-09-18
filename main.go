3801d80715c64c6ca1fb51e06596a8d141132ef0
zhufucdev <1095291434@qq.com>
2024-09-18 11:25:13 +0800

    :memo: add example

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sdk_go/sdk"
+)
 
+func main() {
+	q := sdk.UpdateQuery{
+		Product:        "turret",
+		Os:             sdk.Android,
+		CurrentVersion: "1.0.0",
+	}
+	a, err := sdk.GetReleaseAsset("http://localhost:3000/api", q)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if a != nil {
+		fmt.Println(a.VersionName, a.Url)
+	} else {
+		fmt.Println("No update found.")
+	}
 }
